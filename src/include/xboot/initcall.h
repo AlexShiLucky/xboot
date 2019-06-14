c54271075de62da48d41d750959836c33a17f7fb
AlexShi <shiweining123@163.com>
2019-06-14 16:30:58 +0800

    [修改] 1.修改initcall和exitcall相关宏定义.
    Signed-off-by: AlexShi <shiweining123@163.com>

--- src/include/xboot/initcall.h
+++ src/include/xboot/initcall.h
@@ -11,35 +11,58 @@ typedef void (*exitcall_t)(void);
 #define __init __attribute__ ((__section__ (".init.text")))
 #define __exit __attribute__ ((__section__ (".exit.text")))
 
-#define __define_initcall(level, fn, id) \
-    static const initcall_t __initcall_##fn##id \
-    __attribute__((__used__, __section__(".initcall_" level ".text"))) = fn
+#define __define_initcall(fn, level) \
+    static const initcall_t __initcall_##fn##level \
+    __attribute__((__used__, __section__(".initcall_" #level ".text"))) = fn
 
-#define __define_exitcall(level, fn, id) \
-    static const exitcall_t __exitcall_##fn##id \
-    __attribute__((__used__, __section__(".exitcall_" level ".text"))) = fn
+#define __define_exitcall(fn, level) \
+    static const exitcall_t __exitcall_##fn##level \
+    __attribute__((__used__, __section__(".exitcall_" #level ".text"))) = fn
 
-#define pure_initcall(fn)       __define_initcall("0", fn, 0)   // 第0级初始化调用段
-#define machine_initcall(fn)    __define_initcall("1", fn, 1)   // 第1级初始化调用段
-#define core_initcall(fn)       __define_initcall("2", fn, 2)   // 第2级初始化调用段
-#define postcore_initcall(fn)   __define_initcall("3", fn, 3)   // 第3级初始化调用段
-#define driver_initcall(fn)     __define_initcall("4", fn, 4)   // 第4级初始化调用段
-#define subsys_initcall(fn)     __define_initcall("5", fn, 5)   // 第5级初始化调用段
-#define command_initcall(fn)    __define_initcall("6", fn, 6)   // 第6级初始化调用段
-#define server_initcall(fn)     __define_initcall("7", fn, 7)   // 第7级初始化调用段
-#define reserver_initcall(fn)   __define_initcall("8", fn, 8)   // 第8级初始化调用段
-#define late_initcall(fn)       __define_initcall("9", fn, 9)   // 第9级初始化调用段
+#define initcall_pure(fn)       __define_initcall(fn, 0)   // 第0级初始化调用段
+#define initcall_machine(fn)    __define_initcall(fn, 1)   // 第1级初始化调用段
+#define initcall_core(fn)       __define_initcall(fn, 2)   // 第2级初始化调用段
+#define initcall_postcore(fn)   __define_initcall(fn, 3)   // 第3级初始化调用段
+#define initcall_driver(fn)     __define_initcall(fn, 4)   // 第4级初始化调用段
+#define initcall_subsys(fn)     __define_initcall(fn, 5)   // 第5级初始化调用段
+#define initcall_command(fn)    __define_initcall(fn, 6)   // 第6级初始化调用段
+#define initcall_server(fn)     __define_initcall(fn, 7)   // 第7级初始化调用段
+#define initcall_reserver(fn)   __define_initcall(fn, 8)   // 第8级初始化调用段
+#define initcall_late(fn)       __define_initcall(fn, 9)   // 第9级初始化调用段
 
-#define pure_exitcall(fn)       __define_exitcall("0", fn, 0)   // 第0级退出调用段
-#define machine_exitcall(fn)    __define_exitcall("1", fn, 1)   // 第1级退出调用段
-#define core_exitcall(fn)       __define_exitcall("2", fn, 2)   // 第2级退出调用段
-#define postcore_exitcall(fn)   __define_exitcall("3", fn, 3)   // 第3级退出调用段
-#define driver_exitcall(fn)     __define_exitcall("4", fn, 4)   // 第4级退出调用段
-#define subsys_exitcall(fn)     __define_exitcall("5", fn, 5)   // 第5级退出调用段
-#define command_exitcall(fn)    __define_exitcall("6", fn, 6)   // 第6级退出调用段
-#define server_exitcall(fn)     __define_exitcall("7", fn, 7)   // 第7级退出调用段
-#define reserver_exitcall(fn)   __define_exitcall("8", fn, 8)   // 第8级退出调用段
-#define late_exitcall(fn)       __define_exitcall("9", fn, 9)   // 第9级退出调用段
+#define pure_initcall(fn)       initcall_pure(fn)
+#define machine_initcall(fn)    initcall_machine(fn)
+#define core_initcall(fn)       initcall_core(fn)
+#define postcore_initcall(fn)   initcall_postcore(fn)
+#define driver_initcall(fn)     initcall_driver(fn)
+#define subsys_initcall(fn)     initcall_subsys(fn)
+#define command_initcall(fn)    initcall_command(fn)
+#define server_initcall(fn)     initcall_server(fn)
+#define reserver_initcall(fn)   initcall_reserver(fn)
+#define late_initcall(fn)       initcall_late(fn)
+
+#define exitcall_pure(fn)       __define_exitcall(fn, 0)   // 第0级退出调用段
+#define exitcall_machine(fn)    __define_exitcall(fn, 1)   // 第1级退出调用段
+#define exitcall_core(fn)       __define_exitcall(fn, 2)   // 第2级退出调用段
+#define exitcall_postcore(fn)   __define_exitcall(fn, 3)   // 第3级退出调用段
+#define exitcall_driver(fn)     __define_exitcall(fn, 4)   // 第4级退出调用段
+#define exitcall_subsys(fn)     __define_exitcall(fn, 5)   // 第5级退出调用段
+#define exitcall_command(fn)    __define_exitcall(fn, 6)   // 第6级退出调用段
+#define exitcall_server(fn)     __define_exitcall(fn, 7)   // 第7级退出调用段
+#define exitcall_reserver(fn)   __define_exitcall(fn, 8)   // 第8级退出调用段
+#define exitcall_late(fn)       __define_exitcall(fn, 9)   // 第9级退出调用段
+
+#define pure_exitcall(fn)       exitcall_pure(fn)
+#define machine_exitcall(fn)    exitcall_machine(fn)
+#define core_exitcall(fn)       exitcall_core(fn)
+#define postcore_exitcall(fn)   exitcall_postcore(fn)
+#define driver_exitcall(fn)     exitcall_driver(fn)
+#define subsys_exitcall(fn)     exitcall_subsys(fn)
+#define command_exitcall(fn)    exitcall_command(fn)
+#define server_exitcall(fn)     exitcall_server(fn)
+#define reserver_exitcall(fn)   exitcall_reserver(fn)
+#define late_exitcall(fn)       exitcall_late(fn)
+
 
 void do_initcalls(void);
 void do_exitcalls(void);

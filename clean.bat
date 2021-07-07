4d6509a8b0ff122addb96fd0c464c9a1ad89d3c5
AlexShi <shiweining123@163.com>
2021-07-07 15:19:39 +0800

    [修改] 1.修改交叉工具链版本及位置.
    Signed-off-by: AlexShi <shiweining123@163.com>

--- clean.bat
+++ clean.bat
@@ -1,3 +1,6 @@
 @set ECLIPSE_DIR=C:\Eclipse\eclipse-for-xboot-windows-x86_64
-@set PATH=%ECLIPSE_DIR%\compiler\bin;%ECLIPSE_DIR%\compiler\arm-none-eabi\bin;%ECLIPSE_DIR%\compiler\arm-none-linux-gnueabihf\bin;%ECLIPSE_DIR%\compiler\aarch64-none-elf\bin;%ECLIPSE_DIR%\compiler\aarch64-none-linux-gnu\bin;%ECLIPSE_DIR%\compiler\riscv-none-embed\bin;%PATH%;
+@set TOOLCHAIN_DIR=C:\toolchain
+@set GNU_ARM=%TOOLCHAIN_DIR%\gnu_arm
+@set GNU_LINARO=%TOOLCHAIN_DIR%\gnu_linaro
+@set PATH=%ECLIPSE_DIR%\compiler\bin;%GNU_ARM%\arm-none-eabi\bin;%GNU_ARM%\arm-none-linux-gnueabihf\bin;%GNU_ARM%\aarch64-none-elf\bin;%GNU_ARM%\aarch64-none-linux-gnu\bin;%GNU_LINARO%\arm-eabi\bin;%GNU_LINARO%\arm-linux-gnueabihf\bin;%GNU_LINARO%\aarch64-elf\bin;%GNU_LINARO%\aarch64-linux-gnu\bin;%TOOLCHAIN_DIR%\riscv-none-embed\bin;%PATH%;
 @make clean
